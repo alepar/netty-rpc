95c48fc0353570d49a61ea6c9ee2bd729dfb104c
Alexey Parfenov <alepar@gmail.com>
2011-05-11 00:10:57 +0400

    even more clarification about exceptions

--- rpc/src/java/main/ru/alepar/rpc/exception/ProtocolException.java
+++ rpc/src/java/main/ru/alepar/rpc/exception/ProtocolException.java
@@ -2,7 +2,9 @@ package ru.alepar.rpc.exception;
 
 /**
  * This exception is thrown, when one of protocol constraints is violated (eg trying to pass non-serializable object through rpc). <br/>
- * If you see this exceptions thrown in your app - it means there's something wrong with your code
+ * If you see this exceptions thrown in your app - it means there's something wrong with your code <br/>
+ * <br/>
+ * When you get this exception it is pointless to retry rpc calls - they will always fail.
  */
 public class ProtocolException extends RuntimeException {
 
