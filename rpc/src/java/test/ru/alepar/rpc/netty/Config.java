39e6ae19b71311ec2350b5be071e9b649b094f1c
alepar <alepar@gmail.com>
2011-10-27 13:03:37 +0400

    increasing timeout - allow more breathing space for async tests to complete

--- rpc/src/java/test/ru/alepar/rpc/netty/Config.java
+++ rpc/src/java/test/ru/alepar/rpc/netty/Config.java
@@ -3,6 +3,6 @@ package ru.alepar.rpc.netty;
 import java.net.InetSocketAddress;
 
 public class Config {
-    public static final InetSocketAddress BIND_ADDRESS = new InetSocketAddress(8080);
-    public static final long TIMEOUT = 1000l;
+    public static final InetSocketAddress BIND_ADDRESS = new InetSocketAddress(8338);
+    public static final long TIMEOUT = 2000l;
 }
